02b973a9dbc4487d0431d3cafd9817ab4f60ab5f
JackSkerrett724 <jackskerrett23@gmail.com>
2023-11-22 17:26:53 -0500

    calculating distances for each line

--- pathfinding.py
+++ pathfinding.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
+import math
 
 #263 pixels = 224 inches
 #1 Pixel = .85 inches
 P2I = lambda p: p*.85 ## Convert Pixels to Inches
 pointList = []
+distances = []
 def CreateWindow():
     global img
     #img = np.zeros((512, 512, 1), dtype = "uint8")
     img = cv2.imread("2023Field.png")
-    #img = cv2.resize(img, None, fx=.1, fy=.1)
-    img = cv2.resize(img, (768, 372))
+    img = cv2.resize(img, (768, 372)) 
     img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
     cv2.imshow("Window", img )
 
@@ -21,21 +22,21 @@ def CreatePath(event, x, y, flags, params):
         cv2.imshow("Window", img)
         pointList.append((x,y))
     elif event == cv2.EVENT_RBUTTONDOWN:
-        print("Change:")
-        print(pointList[1][1] - pointList[0][1])
+        #print(pointList[1][1] - pointList[0][1])
         for index, point in enumerate(pointList):
             if index+1 < len(pointList):
                 cv2.line(img, point, pointList[index+1], (0,225,0), 2)
                 cv2.imshow("Window", img)
-        #cv2.rectangle(img, (pointList[0][0]-5, pointList[0][1]-5), (pointList[0][0]+5, pointList[0][1]+5), (225, 0, 0), -1)
-        cv2.imshow("Window", img)
-        
+                dx = pointList[index+1][0] - point[0] 
+                dy = pointList[index+1][1] - point[1] 
+                d = math.sqrt(dx**2 + dy**2)
+                distances.append(P2I(d))
+        print(distances)
 
 
 
 def main():
     print("Pathfinding for 1507")
-    print(P2I(263))
     print("Left Click To Place Points, Right Click To Finish Placing Points")
     CreateWindow()
     cv2.setMouseCallback("Window", CreatePath)
