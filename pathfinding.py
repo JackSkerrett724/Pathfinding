bfdb42729416f97ebfa47eb384ceb31ad4be8172
JackSkerrett724 <jackskerrett23@gmail.com>
2023-11-22 15:22:27 -0500

    Create Black Window using cv2

--- pathfinding.py
+++ pathfinding.py
@@ -1,6 +1,15 @@
+from email.mime import image
+import cv2
+import numpy as np
+
+def CreateWindow():
+    img = np.zeros((512, 512, 1), dtype = "uint8")
+    cv2.imshow("Window", img)
+    cv2.waitKey(0)
 
 def main():
     print("Pathfinding for 1507")
+    CreateWindow()
 
 
 if __name__ == "__main__":
