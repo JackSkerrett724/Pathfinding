69d7219bb1e095410b6aa32e57f2870b86f8534a
JackSkerrett724 <jackskerrett23@gmail.com>
2023-11-22 15:42:19 -0500

    Draw points with mouseclick on screen

--- pathfinding.py
+++ pathfinding.py
@@ -2,14 +2,23 @@ import cv2
 import numpy as np
 
 def CreateWindow():
+    global img
     img = np.zeros((512, 512, 1), dtype = "uint8")
     cv2.imshow("Window", img)
-    cv2.waitKey(0)
+
+def CreatePoints(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x, y)
+        cv2.circle(img, (x,y), 1, (225,225,0), -1)
+        cv2.imshow("Window", img)
 
 def main():
     print("Pathfinding for 1507")
     CreateWindow()
+    print("continue")
+    cv2.setMouseCallback("Window", CreatePoints)
 
 
 if __name__ == "__main__":
     main()
+    cv2.waitKey(0)
