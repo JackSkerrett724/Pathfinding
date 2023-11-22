3774b3636f2f8516eea2454ce9b71a7c17b51c68
JackSkerrett724 <jackskerrett23@gmail.com>
2023-11-22 16:15:24 -0500

    Create lines between points

--- pathfinding.py
+++ pathfinding.py
@@ -1,28 +1,34 @@
 import cv2
 import numpy as np
 
-pointList = []
 
+pointList = []
 def CreateWindow():
     global img
     img = np.zeros((512, 512, 1), dtype = "uint8")
     cv2.imshow("Window", img)
 
-def CreatePoints(event, x, y, flags, params):
+def CreatePath(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(x, y)
         cv2.circle(img, (x,y), 1, (225,225,0), -1)
         cv2.imshow("Window", img)
         pointList.append((x,y))
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        for index, point in enumerate(pointList):
+            if index+1 < len(pointList):
+                cv2.line(img, point, pointList[index+1], (225,225,0), 1)
+                cv2.imshow("Window", img)
+
+
 
 def main():
     print("Pathfinding for 1507")
+    print("Left Click To Place Points, Right Click To Finish Placing Points")
     CreateWindow()
-    print("continue")
-    cv2.setMouseCallback("Window", CreatePoints)
+    cv2.setMouseCallback("Window", CreatePath)
 
 
 if __name__ == "__main__":
     main()
     cv2.waitKey(0)
-    print(pointList)
