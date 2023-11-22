e8995c4329740edc07de0c8a0a2c6fa3c9fecd9b
JackSkerrett724 <jackskerrett23@gmail.com>
2023-11-22 16:51:23 -0500

    Brought Image of the Field in

--- pathfinding.py
+++ pathfinding.py
@@ -1,12 +1,17 @@
 import cv2
 import numpy as np
 
-
 pointList = []
 def CreateWindow():
     global img
-    img = np.zeros((512, 512, 1), dtype = "uint8")
-    cv2.imshow("Window", img)
+    #img = np.zeros((512, 512, 1), dtype = "uint8")
+    img = cv2.imread("2023Field.png")
+    print('Image Width is',img.shape[1])
+    print('Image Height is',img.shape[0])
+    img = cv2.resize(img, None, fx=.1, fy=.1)
+    print('Image Width is',img.shape[1])
+    print('Image Height is',img.shape[0])
+    cv2.imshow("Window", img )
 
 def CreatePath(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -19,6 +24,10 @@ def CreatePath(event, x, y, flags, params):
             if index+1 < len(pointList):
                 cv2.line(img, point, pointList[index+1], (225,225,0), 1)
                 cv2.imshow("Window", img)
+        for i in range(5):
+            cv2.rectangle(img, (pointList[0][0]-i, pointList[0][1]-i), (pointList[0][0]+i, pointList[0][1]+i), (225, 0, 0), -1)
+            cv2.imshow("Window", img)
+        
 
 
 
