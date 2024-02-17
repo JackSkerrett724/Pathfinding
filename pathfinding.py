56f2d14ee8a55ef85515b10f648e5a36b410ec1b
JackSkerrett <jackskerrett23@gmail.com>
2024-02-17 16:29:50 -0500

    Angle calc works!!!!!!!

--- pathfinding.py
+++ pathfinding.py
@@ -5,7 +5,10 @@ import math
 
 #1 Pixel = .996 inches
 P2I = lambda p: p*.996 ## Convert Pixels to Inches 
+first = False
+origin = (0,0)
 pointList = []
+relativeList = []
 distances = []
 angles = []
 def CreateWindow():
@@ -16,13 +19,21 @@ def CreateWindow():
     #img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
     cv2.imshow("Window", img )
 
+
 def CreatePath(event, x, y, flags, params):
+    global first, origin
     if event == cv2.EVENT_LBUTTONDOWN:
+        if not first:
+            origin = (x,y)
+            first = True
+            print(f"origin {origin}")
         print(x, y)
         cv2.circle(img, (x,y), 3, (0,225,0), -1)
         cv2.imshow("Window", img)
-        pointList.append((x,y))
+        pointList.append((x ,y))
     elif event == cv2.EVENT_RBUTTONDOWN:
+        for index, point in enumerate(pointList): 
+            relativeList.append(((point[0] - origin[0]), -(point[1] - origin[1])))
         #print(pointList[1][1] - pointList[0][1])
         for index, point in enumerate(pointList):
             if index+1 < len(pointList):
@@ -34,18 +45,22 @@ def CreatePath(event, x, y, flags, params):
                 d = math.sqrt(dx**2 + dy**2)
                 distances.append(P2I(d)) ## distances in Inches that the robot can use
                 #################################
-                #########ANGLE CALC##############
-                Y1 = max(pointList[index+1][1] , point[1])
-                Y2 = min(pointList[index+1][1] , point[1] )
-                numer = Y1 - Y2
-                dx = point[0] - pointList[index+1][0]
-                denom = math.sqrt(dx**2 + numer**2)
-                frac = numer/denom
-                theta = math.acos(frac) * (180.0 / math.pi)
+        #########ANGLE CALC##############
+        for index, point in enumerate(relativeList):
+            if index+1 < len(relativeList):
+                dx = relativeList[index+1][0] - point[0] 
+                dy = relativeList[index+1][1] - point[1] 
+                sqrt = math.sqrt((dx**2) + (dy**2))   
+                print(dx, dy, "hi")
+                theta = math.acos(dx/sqrt) ## Theta in radians because python math hates me
+                theta = math.degrees(theta) ## convert to degrees
+                if(dy < 0):
+                    theta = 360 - theta
                 angles.append(theta)
-                ######################
+        ######################
         print(distances)
         print(angles)
+        print(relativeList)
 
 
 
